fa596e97cf8721999c592f0d62051d3d54de202e
jx-dohwan <jx7789@gmail.com>
2024-05-06 15:27:58 +0900

    feat.header component unit testing

---
+++ src/components/__tests__/header.spec.tsx
@@ -0,0 +1,78 @@
+import { MockedProvider } from "@apollo/client/testing";
+import { render, screen, waitFor } from "@testing-library/react";
+import { ME_QUERY } from "../../hooks/useMe";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Header } from "../header";
+
+describe("<Header />", () => {
+
+    it("renders verify banner", async () => {
+        // GraphQL 쿼리 모킹 데이터 
+        render(
+            <MockedProvider
+                mocks={[
+                    {
+                        request: {
+                            query: ME_QUERY,
+                        },
+                        result: {
+                            data: {
+                                me: {
+                                    id: 1,
+                                    email: "",
+                                    role: "",
+                                    verified: false, // 이메일이 검증되지 않은 상태
+                                },
+                            },
+                        },
+                    },
+                ]}
+            >
+                {/* Router로 감싸서 Header 컴포넌트의 라우팅 기능을 활성화 */}
+                <Router>
+                    <Header />
+                </Router>
+            </MockedProvider>
+        );
+        await waitFor(async () => {  // 아래의 텍스트가 렌더링 되는지 확인
+            // 모킹된 응답을 기다리기 위해 잠시 지연
+            await new Promise((resolve) => setTimeout(resolve, 5));
+            screen.getByText("이메일을 확인해 주세요.");
+        });
+    });
+
+    it("renders without verify banner", async () => {
+        // GraphQL 쿼리 모킹 데이터
+        render(
+            <MockedProvider
+                mocks={[
+                    {
+                        request: {
+                            query: ME_QUERY,
+                        },
+                        result: {
+                            data: {
+                                me: {
+                                    id: 1,
+                                    email: "",
+                                    role: "",
+                                    verified: true, // 이메일이 검증된 상태
+                                },
+                            },
+                        },
+                    },
+                ]}
+            >
+                 {/* Router로 감싸서 Header 컴포넌트의 라우팅 기능을 활성화 */}
+                <Router>
+                    <Header />
+                </Router>
+            </MockedProvider>
+        );
+
+        await waitFor(async () => {// 아래의 해당 텍스트가 화면에 없는지 확인
+            await new Promise((resolve) => setTimeout(resolve, 5)); // 모킹된 응답을 기다리기 위해 잠시 지연
+            expect(screen.queryByText("이메일을 확인해 주세요.")).toBeNull();
+        })
+    })
+});
